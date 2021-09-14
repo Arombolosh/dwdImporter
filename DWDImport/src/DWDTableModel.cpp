0213d471350d95ee7145579533eb3f7bb36a157f
Stephan Hirth <stephan.hirth@tu-dresden.de>
2021-09-14 13:44:02 +0200

    Added Table model for table view

---
+++ DWDImport/src/DWDTableModel.cpp
@@ -0,0 +1,6 @@
+#include "DWDTableModel.h"
+
+DWDTableModel::DWDTableModel()
+{
+
+}
---
+++ DWDImport/src/DWDTableModel.h
@@ -0,0 +1,12 @@
+#ifndef DWDTABLEMODEL_H
+#define DWDTABLEMODEL_H
+
+#include <QObject>
+
+class DWDTableModel
+{
+public:
+	DWDTableModel();
+};
+
+#endif // DWDTABLEMODEL_H
