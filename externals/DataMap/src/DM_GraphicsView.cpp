c48f80ee0f315254d43e6646da50a346e18fbe6b
Stephan Hirth <stephan.hirth@tu-dresden.de>
2023-06-22 15:09:40 +0200

    Updated qt graphics view

--- externals/DataMap/src/DM_GraphicsView.cpp
+++ externals/DataMap/src/DM_GraphicsView.cpp
@@ -14,26 +14,26 @@ GraphicsView::~GraphicsView() {
 }
 
 void GraphicsView::wheelEvent(QWheelEvent * event) {
-	if (event->modifiers() & Qt::ControlModifier) {
-		// zoom
-		const ViewportAnchor anchor = transformationAnchor();
-		setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
-		int angle = event->angleDelta().y();
-		qreal factor;
-		if (angle > 0) {
-			factor = 1.1;
-		} else {
-			factor = 0.9;
-		}
-		scale(factor, factor);
-		setTransformationAnchor(anchor);
+
+	// zoom
+	const ViewportAnchor anchor = transformationAnchor();
+	setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
+	int angle = event->angleDelta().y();
+	qreal factor;
+	if (angle > 0) {
+		factor = 1.1;
 	} else {
-		QGraphicsView::wheelEvent(event);
+		factor = 0.9;
 	}
+	scale(factor, factor);
+	setTransformationAnchor(anchor);
+
+	return;
+
+	QGraphicsView::wheelEvent(event);
 }
 
 void GraphicsView::resizeEvent(QResizeEvent * event) {
-	//fitInView(scene()->sceneRect(), Qt::KeepAspectRatio);
 	setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
 }
 }
